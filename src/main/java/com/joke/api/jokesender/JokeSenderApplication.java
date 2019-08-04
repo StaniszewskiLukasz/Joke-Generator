be967a8c531d4f40b4e8d572a4443e2b4a816888
Mateusz Slawniewicz <mateusz.slawniewicz@gmail.com>
2019-08-04 09:54:34 +0200

    first initial commit

---
+++ src/main/java/com/joke/api/jokesender/JokeSenderApplication.java
@@ -0,0 +1,13 @@
+package com.joke.api.jokesender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JokeSenderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JokeSenderApplication.class, args);
+    }
+
+}
